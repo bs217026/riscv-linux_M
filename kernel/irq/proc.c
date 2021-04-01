899812b68b889dda342c84c671d6def72b66d8d1
bs217026 <bs217026@sun.ac.jp>
2021-04-01 09:07:39 +0900

    変更３

--- kernel/irq/proc.c
+++ kernel/irq/proc.c
@@ -331,7 +331,7 @@ static int irq_spurious_proc_show(struct seq_file *m, void *v)
 {
 	struct irq_desc *desc = irq_to_desc((long) m->private);
 
-	seq_printf(m, "count %u\n" "unhandled %u\n" "last_unhandled %u ms\n",
+	seq_printf(m, "COUNT %u\n" "UNHANDLED %u\n" "last_unhandled %u ms\n",
 		   desc->irq_count, desc->irqs_unhandled,
 		   jiffies_to_msecs(desc->last_unhandled));
 	return 0;
